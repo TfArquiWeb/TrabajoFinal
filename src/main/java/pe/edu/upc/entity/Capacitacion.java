516765f68b35d8bda54815ec05dd58f855d7a334
TheDiegox980 <83880331+TheDiegox980@users.noreply.github.com>
2022-10-24 15:45:19 -0500

    1

---
+++ src/main/java/pe/edu/upc/entity/Capacitacion.java
@@ -0,0 +1,37 @@
+package pe.edu.upc.entity;
+
+import javax.persistence.*;
+@Entity
+@Table(name = "Capacitacion")
+public class Capacitacion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "descCapacitacion",length = 45,nullable = false)
+    private String descCapacitacion;
+
+    public Capacitacion() {
+    }
+
+    public Capacitacion(int id, String descCapacitacion) {
+        this.id = id;
+        this.descCapacitacion = descCapacitacion;
+    }
+
+    public int getIdCapacitacion() {
+        return id;
+    }
+
+    public void setIdCapacitacion(int idPropietario) {
+        this.id = idPropietario;
+    }
+
+    public String getDescCapacitacion() {
+        return descCapacitacion;
+    }
+
+    public void setDescCapacitacion(String descCapacitacion) {
+        this.descCapacitacion = descCapacitacion;
+    }
+
+}
---
+++ src/main/java/pe/edu/upc/serviceimpl/CapacitacionServiceImpl.java
@@ -0,0 +1,25 @@
+package pe.edu.upc.serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.entity.Capacitacion;
+import pe.edu.upc.repository.ICapacitacionRepository;
+import pe.edu.upc.serviceinterfeaces.ICapacitacionService;
+
+import java.util.List;
+@Service
+public class CapacitacionServiceImpl implements ICapacitacionService {
+    @Autowired
+    private ICapacitacionRepository cR;
+
+    @Override
+    public void insert(Capacitacion capacitacion) {cR.save(capacitacion);}
+
+    @Override
+    public List<Capacitacion> list() {return cR.findAll();}
+
+    @Override
+    public void delete(int id) {cR.deleteById(id);}
+    @Override
+    public List<Capacitacion> search(String descCapacitacion) {return cR.BuscarNombre(descCapacitacion);}
+}
