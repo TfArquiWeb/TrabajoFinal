5d75bea7614f5aa7b7045ed37a0653a96753a5d0
RenzzoGutierrez20 <u202014777@upc.edu.pe>
2022-11-07 03:58:56 -0600

    change

--- src/main/java/pe/edu/upc/controller/ContratoController.java
+++ src/main/java/pe/edu/upc/controller/ContratoController.java
@@ -1,6 +1,15 @@
 package pe.edu.upc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.entity.Contrato;
 import pe.edu.upc.serviceinterfeaces.IContratoInterfaces;
@@ -11,7 +20,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/Contrato")
+@RequestMapping("/contrato")
 public class ContratoController {
     @Autowired
     private IContratoInterfaces cService;
@@ -31,6 +40,7 @@ public class ContratoController {
     public List<Contrato> listar(){
         return cService.listar();
     }
+
     /*@PostMapping("/buscar")
     public List<Contrato> buscar(@RequestBody Contrato c) throws ParseException{
         return null;
