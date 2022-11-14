ca8ab5ad34914149558235c971777768fe7710a8
Aulas y laboratorios <aulasylaboratorioscsi@gmail.com>
2022-11-14 15:08:34 -0500

    Buscar dentro de Curriculum

--- src/main/java/pe/edu/upc/serviceimpl/CurriculumImpl.java
+++ src/main/java/pe/edu/upc/serviceimpl/CurriculumImpl.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.upc.entity.Capacitacion;
+import pe.edu.upc.entity.contador;
 import pe.edu.upc.repository.ICurriculumRepository;
 import pe.edu.upc.serviceinterfeaces.ICurriculumService;
 import pe.edu.upc.entity.Curriculum;
